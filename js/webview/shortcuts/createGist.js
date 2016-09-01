3a738048f88bd6d54f5ae3b2953c17e7f01e6265
Benjamin Butschell <ichbingenial@gmail.com>
2016-09-02 00:01:14 +0200

    better elementary os styles and first working global shortcut

---
+++ js/webview/shortcuts/createGist.js
@@ -0,0 +1,5 @@
+document.querySelector('.sidebar-new-gist').click()
+window.setTimeout(function ()
+{
+    document.querySelector('.new-gist-modal .gist-description').focus();
+}, 500);
--- renderer.js
+++
@@ -1,5 +0,0 @@
-//var cssLink = document.createElement("link");
-//cssLink.href = "file://${__dirname}/css/stryles.css";
-//cssLink.rel = "stylesheet";
-//cssLink.type = "text/css";
-//document.getElementsByTagName('iframe')[0].contentDocument.body.appendChild(cssLink);
