b660a0397271735c594a577e646a19350606cc9e
butu <bb@webprofil.at>
2016-09-01 17:00:01 +0200

    Initial commit

---
+++ renderer.js
@@ -0,0 +1,5 @@
+//var cssLink = document.createElement("link");
+//cssLink.href = "file://${__dirname}/css/stryles.css";
+//cssLink.rel = "stylesheet";
+//cssLink.type = "text/css";
+//document.getElementsByTagName('iframe')[0].contentDocument.body.appendChild(cssLink);
